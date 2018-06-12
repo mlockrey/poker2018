9f8a999b70c2d6ad6c9854847030c43afb24bdea
Michael Lockrey <mlockrey@Michaels-MacBook-Pro.local>
2018-06-12 23:09:43 +1000

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'Deaf Poker Northern Rivers',
+  description: 'Current Australian Champions!',
+  themeConfig: {
+    search: false,
+    nav: [
+      { text: 'Next Game: 15 June 2018', link: 'https://www.facebook.com/groups/162259904505618/' },
+      { text: 'Deaf Poker Australia', link: 'http://deafpoker.com.au' },
+    ]
+  }
+}
